05ef81adf0838bb13a1e8b1db35cdbc5975d980a
zhozhyr <140246410+zhozhyr@users.noreply.github.com>
2024-12-28 13:45:53 +0700

    b

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -25,11 +25,12 @@ urlpatterns = [
     path('pages/', include('pages.urls')),
     path('auth/', include('django.contrib.auth.urls')),
     path(
-         'auth/registration/',
-          views.RegisterView.as_view(),
-          name='registration'
-          )
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        'auth/registration/',
+        views.RegisterView.as_view(),
+        name='registration'
+    )
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 handler404 = 'pages.views.page_not_found'
 handler500 = 'pages.views.page_internal_server_error'
